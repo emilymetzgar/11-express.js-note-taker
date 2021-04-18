817068e69f75c0b4a5e407573dfe1a6e6a13fd64
emilymetzgar93 <emilymetzgar93@gmail.com>
2021-04-18 18:37:54 -0500

    fixed get in routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -14,7 +14,13 @@ module.exports = (app) => {
 
   app.get("/api/notes", (req, res) => {
     // Read the db.json file and return all saved notes as JSON
-    res.json(db);
+    //Reads the json file async
+    const dbFile = fs.readFileSync(path.join(__dirname, "db", "db.json"), "utf8");
+    //Parsing the file is necesary to displayed it like a JSON object array
+    const dbFileJSON = JSON.parse(dbFile);
+    //Return the json db file to the user
+    return res.json(dbFileJSON);
+
   });
 
  // API POST Requests
@@ -24,34 +30,20 @@ module.exports = (app) => {
   // (ex. User fills out a reservation request... this data is then sent to the server...
   // Then the server saves the data to the tableData array)
   // ---------------------------------------------------------------------------
-  app.post("/api/notes",  (req, res) => {
+  //app.post("/api/notes",  (req, res) => {
 
-    console.log(req.body)
     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
     // It will do this by sending out the value "true" have a table
     // req.body is available since we're using the body parsing middleware
     // Add unique id to each note
-    db.forEach((obj, i) => {
-      obj.id = i + 1;
-    });
+    //db.forEach((obj, i) => {
+      //obj.id = i + 1;
+    //});
     // Return the new note to the client
-    fs.writeFile("./db/db.json", JSON.stringify(db), () => {
-      res.json(db);
-    });
-  });
+    //fs.writeFile("./db/db.json", JSON.stringify(db), () => {
+     // res.json(db);
+   // });
+  //});
 
-  // // API DELETE Request
-  app.delete("/api/notes/:id", function (req, res) {
-    var id = req.params.id;
-    // Use splice to delete the selected note from the db array
-    db.splice(id - 1, 1);
-    // Reassign id for each note object
-    db.forEach((obj, i) => {
-      obj.id = i + 1;
-    });
-    // Return the remaining notes to the client
-    fs.writeFile("./db/db.json", JSON.stringify(db), function () {
-      res.json(db);
-    });
-  });
+
 };
